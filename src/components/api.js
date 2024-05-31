bcecc437470d14d4347a793eb4401222de619299
Daryl <daryl@clubdeuce.net>
2024-05-31 13:17:42 -0400

    Implement additional api methods

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,42 @@
 import axios from "axios";
 
+
 const baseUrl = "https://db.swdrenewedhope.com/api";
-const getCard = async cardId => {
+
+/**
+ *
+ * @param cardId
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
+const getCard = cardId => {
     return axios.get(`${baseUrl}/public/card/${cardId}`);
 }
 
-export {getCard}
+/**
+ *
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
+const getSets = () => {
+    return axios.get(`${baseUrl}/public/sets`);
+}
+
+/**
+ *
+ * @param setId
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
+const getSet = setId => {
+    setId = undefined !== setId && setId !== '0' ? setId : '';
+    console.log(`${baseUrl}/public/cards/${setId}`);
+    return axios.get(`${baseUrl}/public/cards/${setId}`);
+}
+
+/**
+ *
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
+const getCards = () => {
+    return axios.get(`${baseUrl}/public/cards`);
+}
+
+export {getCard, getSets, getSet, getCards}
