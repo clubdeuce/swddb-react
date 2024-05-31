b3fc1cc8ef9f47feda4b4a899417c33e003c4233
Daryl <daryl@clubdeuce.net>
2024-05-31 13:24:14 -0400

    Implement generic card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,22 @@
+import CardHeader from "./CardHeader";
 
+const Card = ({label, faction, affiliation, rarity, subtype}) => {
+    subtype = subtype !== undefined ? subtype : [];
+    const renderedSubtypes = subtype.map((subtype, index) => {
+        return <li key={index + 1}>{subtype.name}</li>;
+    });
+
+    return <div className="card">
+        <CardHeader
+            label={label}
+            affiliation={affiliation}
+            faction={faction}
+            rarity={rarity}
+        />
+        <ul className="card__keywords">
+            {renderedSubtypes}
+        </ul>
+    </div>
+}
+
+export default Card;
